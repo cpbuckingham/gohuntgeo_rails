7c8ee56d413ffc0d891569eaf29dedb60a7a779a
Cameron <cameron.p.buckingham@gmail.com>
2014-09-24 14:00:09 -0600

    nada

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,32 @@
 class UserController < ApplicationController
 
+  def show
+    @user = User.find(params[:id])
+  end
 
+  def update
+    x_forwarded_ip = request.env['HTTP_X_FORWARDED_FOR']
+    ip = request.remote_ip
+
+    if get_my_location(ip).nil?
+      ip = '74.125.113.104'
+    end
+
+    remote_ip_location = get_my_location(ip)
+    if x_forwarded_ip.present?
+      @location = get_my_location(x_forwarded_ip.split(',')[0])
+      if @location.nil?
+        @location = remote_ip_location
+      end
+    else
+      @location = remote_ip_location
+    end
+  end
+
+  private
+
+  def get_my_location(ip)
+    SimpleGeolocation::Geocoder.new(ip).geocode!
+  end
 end
+
